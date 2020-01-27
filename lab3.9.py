5d6ffc96a2cc95f7088344a9c6b70ae800544bd6
AVA-KIT <gazda.edu@gmail.com>
2020-01-27 10:59:13 +0100

    Zadanie 9

---
+++ lab3.9.py
@@ -0,0 +1,28 @@
+x = int(input('Podaj wartośc: '))
+a = 3*x**3 - 2*x**2 + 3*x -1
+print(a)
+
+from math import pi
+x = int(input('Podaj wartośc: '))
+
+b = x**2
+c = pi**2
+d = x**2 + 0.5
+e = c*d
+f = b/e
+g = 1
+h = (x**2 - 0.5)**2
+i = c*h
+j = b / i
+
+print(f*(1+h))
+
+x = int(input('Podaj wartośc: '))
+
+k = 1
+l = x + 1/x
+m = x + 1 / l
+n = x + 1/ m
+o = k / n
+
+print(o)
