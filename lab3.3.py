2b71dd2b51234b0d5c55ed4b2da6a226d1ec86c7
AVA-KIT <gazda.edu@gmail.com>
2020-01-27 10:53:34 +0100

    Zadanie 3

---
+++ lab3.3.py
@@ -0,0 +1,9 @@
+while True:
+    a = input('Proszę wcisnąć literę "n" lub "c"')
+    if a == 'n' or a == 'c':
+        print('Dziękuję')
+        break
+    else:
+        print ('Niewłaściwa litera, spróbuj jescze raz')
+
+        
